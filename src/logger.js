dd997fc01267b566eb8f88e0921426e6124b03df
Julian <dreerr@gmail.com>
2022-01-13 21:23:13 +0100

    fancy logger

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,28 @@
 import winston from "winston"
+
+
 export const logger = winston.createLogger({
-  level: "info",
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "./log/combined.log" }),
+    new winston.transports.Console({
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({
+      filename: "log/combined.log",
+      level: "info",
+    }),
+    new winston.transports.File({
+      filename: "log/errors.log",
+      level: "error",
+    }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: "log/exceptions.log" }),
   ],
 })
+winston.exceptions.handle(
+  new winston.transports.File({ filename: "log/exceptions.log" })
+)
