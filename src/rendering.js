763f16eb71253753654c1d9368d0da839ec226db
Julian <dreerr@gmail.com>
2022-01-25 18:29:30 +0100

    fail on initialize if binary not found

--- src/rendering.js
+++ src/rendering.js
@@ -3,13 +3,16 @@ import path from "path"
 import { exec } from "child_process"
 import { fileURLToPath } from "url"
 import { logger } from "./logger.js"
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const resvg = `${__dirname}/../lib/resvg_${process.platform}`
+if (!fs.existsSync(resvg)) {
+  throw new Error("resvg binary not found")
+}
 
 export const rendering = (target, opts) =>
   new Promise((resolve, reject) => {
-    const targetRender = target.replace(/\.[^/.]+$/, "") + ".png"
-    const resvg = `${__dirname}/../lib/resvg_${process.platform}`
-    if (!fs.existsSync(resvg)) reject(Error("resvg binary not found"))
+    const targetRender = target.replace(/\.[^.]+$/, "") + ".png"
     const renderSize = opts.renderSize || 2000
     exec(
       `"${resvg}" "${target}" "${targetRender}" -w ${renderSize}`,
